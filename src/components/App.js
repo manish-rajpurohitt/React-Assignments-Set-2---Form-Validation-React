05f89f1487cf917dc83c6ab947c54476cbd6b290
Manish Purohit <manishkumar9866@gmail.com>
2020-11-28 12:05:35 +0530

    donee

--- src/components/App.js
+++ src/components/App.js
@@ -36,47 +36,36 @@ const App = () => {
   }
   return (
     <div id="main">
-      
-       <label>
           Name:{" "}
           <input  
             type="text" 
             value={formState.name} 
             onChange={(e)=>setFormState({...formState,name:e.target.value})} 
             data-testid = 'name' />
-        </label>
-        <label>
-          Email:
+          Email:{" "}
           <input  
             type="text" 
             value={formState.email} 
             onChange={(e)=>setFormState({...formState,email:e.target.value})}
             data-testid = 'email' />
-        </label>
-        <label data-testid = 'gender'>
           Gender:
           <input 
             data-testid = 'gender'
             type="text" 
             value={formState.gender}
             onChange={(e)=>setFormState({...formState,gender:e.target.value})} />
-        </label>
-        <label>
           Phone Number:
           <input 
             data-testid = 'phoneNumber'
             type="text" 
             value={formState.phone} 
             onChange={(e)=>setFormState({...formState,phone:e.target.value})} />
-        </label>
-        <label>
           Password:
           <input 
             data-testid = 'password'
             type='password'
             value={formState.password} 
             onChange={(e)=>setFormState({...formState,password:e.target.value})} />
-        </label>
         <button 
           data-testid = 'submit'
           type="submit" 
